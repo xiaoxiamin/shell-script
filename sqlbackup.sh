068ee6caa81632209efbdfbb328dc81881f3593b
虾米 <1677661334@qq.com>
2017-06-05 15:58:34 +0800

    加备份失败的报警邮件

--- sqlbackup.sh
+++ sqlbackup.sh
@@ -8,3 +8,9 @@ file_name_qsvc="qsvc_"$(date +%Y-%m-%d-%T)".sql.gz"
 
 
 /usr/bin/mysqldump -uroot -pPassw0rd -h 127.0.0.1 -P 3306 --databases qsvc |gzip -c > ${file_dirname}/${file_name_qsvc}
+
+size=`du -sh ${file_dirname}/${file_name_JiuYangPErp} |awk '{print $1}'|sed 's/.$//'`#取刚备份的压缩文件大小的数字
+if [ $size -lt 10 ]
+        then
+                echo -e "Test Server : $file_name_JiuYangPErp Less than 10K/M, Mysql Backup Failed!" |mail -s "OA Mysql Backup Failed!" xia.min@mesyun.net
+fi
